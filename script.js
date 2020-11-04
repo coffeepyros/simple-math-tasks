4db86d46775aa09a903c72157a71662ce158b0fe
coffeepyros <coffeepyros@gmail.com>
2020-11-04 18:48:45 +0100

    tasks are now objects

--- script.js
+++ script.js
@@ -5,30 +5,40 @@ const nrOfTasks = 15;
 
 function fillAdd() {
   addition.innerText = "";
+  let tasks = [];
   for (let i = 0; i < nrOfTasks; i++) {
     let result = getRndInteger(2, 100);
     let x = getRndInteger(1, result - 1);
-    addFormula(x, result - x, "+");
+    tasks.push({x: x, y: result-x, op: "+"})
+    // addFormula(x, result - x, "+");
   }
+  addTasks(tasks);
+  console.log(tasks)
 }
 
 function fillSub() {
   subtraction.innerText = "";
+  let tasks = [];
   for (let i = 0; i < nrOfTasks; i++) {
     let x = getRndInteger(2, 50);
     let y;
     do {
       y = getRndInteger(1, x);
     } while (x === y);
-    addFormula(x, y, "-");
+    tasks.push({x: x, y: y, op: "-"})
   }
+  addTasks(tasks);
+  console.log(tasks)
 }
 
-function addFormula(x, y, op) {
-  let task = document.createElement("p");
-  task.innerText = x + " " + op + " " + y + " =";
-  if (op === "+") addition.append(task);
-  else if (op === "-") subtraction.append(task);
+function addTasks(tasks) {
+  tasks.map((task)=> {
+    let p = document.createElement("p");
+    p.innerText = task.x + " " + task.op + " " + task.y + " =";
+    if (task.op === "+") addition.append(p);
+    else if (task.op === "-") subtraction.append(p);
+    return null;
+  });
 }
 
 function getRndInteger(min, max) {
